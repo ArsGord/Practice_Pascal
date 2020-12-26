6f12fbcc3e2d28afcb0d54c59da54fbfdc49672a
ArsGord <67157452+ArsGord@users.noreply.github.com>
2020-12-26 13:28:39 +0400

    Add files via upload

---
+++ 1. в) Сумма синусов от x, взятых n раз.pas
@@ -0,0 +1,17 @@
+﻿var n, i: integer; x, s: real;
+begin
+  writeln ('Введите n:'); // Ввод значений n и x с клавиатуры
+  readln (n);
+  writeln ('Введите x:');
+  readln (x);
+  
+  s:= 0; // Сумма
+  for i:= 1 to n do
+  begin
+    x:= sin(x); // Записываем в переменную x синус от предыдущего члена ряда
+    s:= s + x // Подсчитываем сумму ряда
+  end;
+  
+  writeln ('Ответ:'); // Выводим ответ
+  write (s);
+end.
